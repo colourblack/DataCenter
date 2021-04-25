a53c4a9fc7b9301b2fbdf48986a316167e0835e4
colour_black <932805847@qq.com>
2021-04-25 11:12:46 +0800

    pom文件和Spring启动类

---
+++ src/main/java/com/yingf/Application.java
@@ -0,0 +1,18 @@
+package com.yingf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yf-Fangjunjin
+ * @version 1.0
+ * @since 4/25/21 11:11 AM
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
